04a75f4a73dcdb764561e19d8e0df381ca451eb9
stephenchan9 <stephen.chan9@yahoo.com>
2020-07-13 13:45:23 -0700

    Making the new homepage with the permannet drawer component. Includes the draft board and play search components on the single page.

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,33 +1,144 @@
-import React from "react";
-import PermanentDrawerLeft from "../components/material-assets/drawers/permDrawer.js";
+/*
+    Permnanent drawer
+    https://material-ui.com/components/drawers/#drawer
+
+    keep this as a functional component in order to allow useStyles function to work. useStyles is a hook api 
+    and cannot be used inside classes.
+*/
+
+import React, {useEffect} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from '@material-ui/core/Grid';
 import PropTypes from "prop-types";
 
-class Homepage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// Icons from material
+import ListAltOutlinedIcon from "@material-ui/icons/ListAltOutlined";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
+import SettingsApplicationsOutlinedIcon from "@material-ui/icons/SettingsApplicationsOutlined";
 
-  // called when the component is rendered
-  componentDidMount() {
-    console.log("Finished Mounting Homepage");
-  }
+// imported components
+import DraftBoardContainer from "../containers/draftboardcontainer";
+import PlayerSearchContainer from "../containers/playersearchcontainer.js";
 
-  render() {
-    const draftBoardProps = {
-      players: [],
-      heading: `Welcome ${this.props.name || "User 1"}`
-    }
+const drawerWidth = 240;
 
-    return (
-      <React.Fragment>
-        <PermanentDrawerLeft draftBoard={draftBoardProps}/>
-      </React.Fragment>
-    );
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexGrow: 1,
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function PermanentDrawerLeft(props) {
+  const classes = useStyles();
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log("Homepage is mounted");
+  });
+ 
+  // sections to build in the drawers.
+  const sections = [
+    { text: "Home", icon: <HomeOutlinedIcon /> },
+    { text: "Boards", icon: <ListAltOutlinedIcon /> },
+    { text: "Players", icon: <PeopleAltOutlinedIcon /> },
+  ];
+
+  const sections2 = [
+    { text: "Settings", icon: <SettingsApplicationsOutlinedIcon /> },
+  ];
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          {/* import the heading from homepage */}
+          <Typography variant="h6" noWrap>
+            {props.heading}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {sections.map((section) => (
+            <ListItem button key={section.text}>
+              <ListItemIcon>{section.icon}</ListItemIcon>
+              <ListItemText primary={section.text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {sections2.map((section) => (
+            <ListItem button key={section.text}>
+              <ListItemIcon>{section.icon}</ListItemIcon>
+              <ListItemText primary={section.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            {/* Import the draft board container */}
+            <DraftBoardContainer players={props.draftBoard} />
+          </Grid>
+          <Grid item xs={6}>
+            {/* Import the player search container */}
+            <PlayerSearchContainer />
+          </Grid>
+        </Grid>
+      </main>
+    </div>
+  );
 }
 
-Homepage.propTypes = {
-  name: PropTypes.string,
-}
-
-export default Homepage;
+PermanentDrawerLeft.propTypes = {
+  heading: PropTypes.string,
+  draftBoard: PropTypes.array.isRequired,
+};
