158d9c0a7ae7be4a8b862876d7793f98f89f764c
stephenchan9 <stephen.chan9@yahoo.com>
2020-07-13 17:35:41 -0700

    Remove theme from rebass and use theme from material-ui Created seperate JSON file to control theme. Its imported in the index.js file at the start

--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Homepage from "./pages/homepage.js";
+import theme from "./theme.json";
 import { ThemeProvider } from "emotion-theming";
-// import theme from "@rebass/preset";
+
 // React redux
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import draftBoard from "./reducers/draftboardreducer.js";
 
 const store = createStore(draftBoard);
-
-const theme = {
-  fontSizes: [12, 14, 16, 24, 32, 48, 64],
-  colors: {
-    primary: "#07c",
-    gray: "#f6f6ff",
-  },
-  buttons: {
-    primary: {
-      color: "white",
-      bg: "primary",
-    },
-    outline: {
-      color: "primary",
-      bg: "transparent",
-      boxShadow: "inset 0 0 0 2px",
-    },
-  },
-};
 
 ReactDOM.render(
   <React.Fragment>
